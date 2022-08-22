fe9a268a9a07544ed9c164f4da124f9b73c96ab7
amrendrakind <vision2010@gmail.com>
2022-08-22 21:45:45 +0530

    Fix Linters issues

--- src/redux/shared/api.js
+++ src/redux/shared/api.js
@@ -1,3 +1,5 @@
-import axios from 'axios';
-const rocketsapi = 'https://api.spacexdata.com/v3/rockets';
-export const getAllRockets = () => axios.get(rocketsapi).then((response) => response.data);
+// import axios from 'axios';
+
+// const rocketsapi = 'https://api.spacexdata.com/v3/rockets';
+
+// export const getAllRockets = () => axios.get(rocketsapi).then((response) => response.data);
