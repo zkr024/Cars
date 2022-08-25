e356f75b6a2f991add58964eba8ea72e88ced1c5
zkr024 <zkr.bm.02403@gmail.com>
2022-08-25 11:40:40 -0500

    details and add car functionality

--- src/redux/cars/details.js
+++ src/redux/cars/details.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const DETAILS = 'cars/cars/DETAILS';
 
-const api = 'http://localhost:3000/cars/';
+const api = 'http://localhost:3000/users/';
 
 const detailReducer = (state = [], action) => {
   switch (action.type) {
@@ -13,8 +13,8 @@ const detailReducer = (state = [], action) => {
   }
 };
 
-const getDetails = (id) => async (dispatch) => {
-  const res = await axios.get(`${api}${id}`);
+const getDetails = (id, carId) => async (dispatch) => {
+  const res = await axios.get(`${api}${id}/cars/${carId}`);
   dispatch({
     type: DETAILS,
     payload: res,
