96553dad5a7428db02d9156e2e102b7fd255e214
amrendrakind <vision2010@gmail.com>
2022-08-29 04:25:51 +0530

    Update: Date selection

--- src/components/appointments/Appointment.js
+++ src/components/appointments/Appointment.js
@@ -5,27 +5,33 @@ import Form from 'react-bootstrap/Form';
 
 import Cities from '../cities/cities';
 import Sellers from '../sellers/sellers';
+// import Date from './Date';
 
-const Appointment = () =>
-// console.log(sellers);
+const Appointment = () => {
 // const { userId, carId } = useParams();
 // console.log('UserId '+ userId);
 // console.log('carId is ' + carId);
   // eslint-disable-next-line implicit-arrow-linebreak
-  (
+  const toDay = new Date().toISOString().substring(0, 10);
+  return (
     <div className="user-container" data-testid="user-a">
       <h1>Appointment Component</h1>
       <Form>
         <Cities />
         <Sellers />
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="formBasicDuration">
           <Form.Label>Duration</Form.Label>
-          <Form.Control type="email" placeholder="Enter duration" />
+          <Form.Control type="email" placeholder="Enter duration" autoComplete="off" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="formBasicBranch">
           <Form.Label>Enter Branch</Form.Label>
-          <Form.Control type="password" placeholder="Enter Branch" />
+          <Form.Control type="text" placeholder="Enter Branch" autoComplete="off" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicDate">
+          <Form.Label>Select Date</Form.Label>
+          <Form.Control type="date" defaultValue={toDay} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -38,4 +44,5 @@ const Appointment = () =>
       </Form>
     </div>
   );
+};
 export default Appointment;
