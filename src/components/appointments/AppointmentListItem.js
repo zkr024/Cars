69889868980af4d428a1ac83d166d3082828f389
amrendrakind <vision2010@gmail.com>
2022-08-31 02:01:03 +0530

    Update: Rendering appointment Lists

--- src/components/appointments/AppointmentListItem.js
+++ src/components/appointments/AppointmentListItem.js
@@ -1,7 +1,32 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-function AppointmentsListItem() {
-  return <h1>AppointmentsListItem</h1>;
+function AppointmentsListItem(props) {
+  const {
+    id, userId, carId, sellerId, cityId, duration, branch, dateFor,
+  } = props;
+
+  return (
+    <li key={id}>
+      {userId}
+      {carId}
+      {sellerId}
+      {cityId}
+      {duration}
+      {branch}
+      {dateFor}
+    </li>
+  );
 }
 
 export default AppointmentsListItem;
+
+AppointmentsListItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  userId: PropTypes.number.isRequired,
+  carId: PropTypes.number.isRequired,
+  sellerId: PropTypes.number.isRequired,
+  cityId: PropTypes.number.isRequired,
+  duration: PropTypes.number.isRequired,
+  branch: PropTypes.string.isRequired,
+  dateFor: PropTypes.string.isRequired,
+};
